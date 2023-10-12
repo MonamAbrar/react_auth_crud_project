f5f08ebf0f78bdf7aabc4d38f24c56a45dde8d54
Monam Abrar <mabrar@soloinsight.com>
2023-10-12 15:50:33 +0500

    0.0.3

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import Register from './Components/Register/Register';
 import Navigate from './Components/Navigate/Navigate';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(true);
   const [showRegister, setShowRegister] = useState(false);
+
+  const toggleView = () => {
+    setShowRegister(!showRegister);
+    setShowLogin(!showLogin);
+  }
 
   return(
     <div className="App">
       <h1>Authentication App</h1>
+      
       {showRegister ? (
         <Register onToggle={() => setShowRegister(false)} />
-      ) : (
+      ) : ''}
+
+      {showLogin ? (
         <Login OnRegisterClick={() => setShowRegister(true)} />
-      )}
+      ): ''}
+
+      <br/>
+      
+      <button onClick={toggleView}>
+        {showLogin ? 'Create a new user' : 'Login with existing user'}
+      </button>
+      
+      <br/>
+      <br/>
+
         <Navigate/>
     </div>
   )
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -43,7 +43,6 @@ return (
     <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
     <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
     <button onClick={handleLogin}>Login</button>
-    <button onClick={() => console.log('Open Register Component')}>Register</button>
   </div>
   );
 };
